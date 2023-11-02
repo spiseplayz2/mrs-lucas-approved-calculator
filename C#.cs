b7d6a433ad43ab4c71c98e1714bf3f500c46a67b
spiseplayz2 <149446967+spiseplayz2@users.noreply.github.com>
2023-11-02 11:23:41 -0500

    Update C#.cs

--- C#.cs
+++ C#.cs
@@ -1,4 +1,3 @@
-
 using System.IO;
 using System;
 
@@ -6,35 +5,52 @@ class Program
 {
     static void Main()
     {
-        
-Console.Write("Enter num1: ");
-double num1 = Convert.ToDouble(Console.ReadLine());
-      if ( num1 < 0 ){
-          Console.WriteLine("Cannot Compute negative numbers");
-          return;
-      }
+        {
+            while( true )
 
-Console.Write("Enter operator: ");
-string op = Console.ReadLine();
+            {
 
-Console.Write("Enter num2: ");
-double num2 = Convert.ToDouble(Console.ReadLine());
-       if ( num2 < 0 ){
-          Console.WriteLine("Cannot Compute negative numbers"); 
-          return;
-      }
+                Console.Write("Enter num1: ");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+                if ( num1 < 0 ) {
+                    Console.WriteLine("Cannot Compute negative numbers, please run again.");
+                    return;
+                }
 
-if (op == "+"){
-     Console.WriteLine(num1 + num2);
-} else if (op == "-"){
-     Console.WriteLine(num1 - num2);
-} else if (op == "/"){
-     Console.WriteLine(num1 / num2);
-} else if (op == "*"){
-     Console.WriteLine(num1 * num2);
-} else {
-     Console.WriteLine("Invalid Operator");
-}
+                Console.Write("Enter operator(/,-,+,*): ");
+                string op = Console.ReadLine();
 
+                Console.Write("Enter num2: ");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+                if ( num2 < 0 ) {
+                    Console.WriteLine("Cannot Compute negative numbers, please run again.");
+                    return;
+                }
+
+                if (op == "+") {
+                    Console.WriteLine(num1 + num2);
+                } else if (op == "-") {
+                    Console.WriteLine(num1 - num2);
+                } else if (op == "/") {
+                    Console.WriteLine(num1 / num2);
+                } else if (op == "*") {
+                    Console.WriteLine(num1 * num2);
+                } else {
+                    Console.WriteLine("Invalid Operator");
+
+                }
+                Console.WriteLine(" do you want to do another sentance y/n");
+
+                string yn = Console.ReadLine();
+                if (yn == "y") {
+                    Console.WriteLine("\nEntering the calculator.");
+                    while(true)
+                    break;
+                } else if (yn == "n") {
+                    Console.WriteLine("ok");
+                    return;
+                }
+            }
+        }
     }
 }
