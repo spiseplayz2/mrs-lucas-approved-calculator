9ad66f3a0d21bcf40fc87fac974c46928c7b30c8
spiseplayz2 <149446967+spiseplayz2@users.noreply.github.com>
2023-11-02 11:28:49 -0500

    Update C#.cs

--- C#.cs
+++ C#.cs
@@ -9,24 +9,28 @@ class Program
             while( true )
 
             {
-
+               // ask for number
                 Console.Write("Enter num1: ");
                 double num1 = Convert.ToDouble(Console.ReadLine());
+
+ // make sure thres no negative, if there is the code stops
                 if ( num1 < 0 ) {
                     Console.WriteLine("Cannot Compute negative numbers, please run again.");
                     return;
                 }
-
+               // asks what type of equation you want to do
                 Console.Write("Enter operator(/,-,+,*): ");
                 string op = Console.ReadLine();
-
+                // asks for 2nd number
                 Console.Write("Enter num2: ");
                 double num2 = Convert.ToDouble(Console.ReadLine());
-                if ( num2 < 0 ) {
+                
+        // makes sure theres no negatives, if there is the code stops
+if ( num2 < 0 ) {
                     Console.WriteLine("Cannot Compute negative numbers, please run again.");
                     return;
                 }
-
+                    // does the math
                 if (op == "+") {
                     Console.WriteLine(num1 + num2);
                 } else if (op == "-") {
@@ -38,8 +42,10 @@ class Program
                 } else {
                     Console.WriteLine("Invalid Operator");
 
+                     // asks if you want to do another problem
+
                 }
-                Console.WriteLine(" do you want to do another sentance y/n");
+                Console.WriteLine(" do you want to do another sentance (y/n)");
 
                 string yn = Console.ReadLine();
                 if (yn == "y") {
